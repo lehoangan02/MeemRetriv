1f5b81e8027749cc6c790a900bf68a69d02c631a
DPTPhatUS <tuanphat51sp@gmail.com>
2025-12-14 18:40:29 +0700

    CORS error fix

--- backend/vectordb/src/main/java/cat/dog/controller/APIController.java
+++ backend/vectordb/src/main/java/cat/dog/controller/APIController.java
@@ -13,6 +13,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -20,6 +21,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = {"http://localhost:5173", "https://localhost:5173"}, allowCredentials = "true")
 public class APIController {
     @GetMapping("/hello")
     public String hello(@RequestParam(defaultValue = "World") String name) {
