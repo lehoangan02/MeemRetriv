6468e1c6a8213e7d94b8fb08a7b5904dbeed6d40
Chris-KH <klowgamervn@gmail.com>
2025-12-14 16:37:16 +0700

    remove /lib from ignore

---
+++ frontend/src/lib/axios.ts
@@ -0,0 +1,28 @@
+import { ENV } from "@/config/env";
+import axios from "axios";
+
+const axiosClient = axios.create({
+  baseURL: ENV.VITE_BACKEND_URL,
+  withCredentials: true,
+  timeoutErrorMessage: "Requests take too long to respond",
+});
+
+axiosClient.interceptors.request.use(
+  (config) => {
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  },
+);
+
+axios.interceptors.response.use(
+  (res) => {
+    return res;
+  },
+  (error) => {
+    return Promise.reject(error);
+  },
+);
+
+export default axiosClient;
